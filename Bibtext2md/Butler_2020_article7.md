e049ae186f7a2b70d4970093565d4edddd4c209b
tomlangbehn <tom.langbehn@posteo.de>
2020-11-29 19:04:15 +0100

    publication list updated

---
+++ Bibtext2md/Butler_2020_article7.md
@@ -0,0 +1,13 @@
+---
+type: 'ARTICLE'
+collection: publications
+author: Butler W, Guðmundsd{\'{o}}ttir L, Logemann K, Langbehn T & Marteinsd{\'{o}}ttir G
+year: '2020'
+title: Egg size and density estimates for three gadoids in Icelandic waters and their implications for the vertical distribution of eggs along a stratified water column
+journal: Journal of Marine Systems
+volume: 204
+pages: 103290
+doi: 10.1016/j.jmarsys.2019.103290
+url: https://doi.org/10.1016/j.jmarsys.2019.103290 https://linkinghub.elsevier.com/retrieve/pii/S0924796319304270
+---
+NA
---
+++ Bibtext2md/bib2md.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(bib2df)
+
+#!> read bib text files
+bib <- bib2df::bib2df("my_pubs.bib") %>% arrange(YEAR,AUTHOR)
+
+
+yamllist <- list()
+yamllist2 <- list()
+
+for (i in 1:nrow(bib)) {
+  no_au <- length(bib$AUTHOR[[i]])
+  
+  authorlist <- bib$AUTHOR[[i]]
+  for (j in 1:no_au) {
+    lastname <- bib$AUTHOR[[i]][j] %>%
+      str_remove(., "[,]") %>%
+      str_remove(., "[.]") %>%
+      word(1)
+    
+    initital <- bib$AUTHOR[[i]][j] %>%
+      str_remove(., "[,]") %>%
+      str_remove(., "[.]") %>%
+      str_split(" ") %>%
+      unlist() %>%
+      .[-1] %>%
+      str_sub(., 1, 1) %>%
+      paste(., sep = "", collapse = "")
+    authorlist[j] <- paste(lastname, initital)
+  }
+  
+  typ <- paste0("type: ", "'", paste(bib$CATEGORY[[i]]),"'")
+  col <- paste0("collection: ", "publications")
+  au <- paste0("author: ", paste0(paste(authorlist[-no_au], collapse = ", ")), " & ", last(authorlist))
+  yr <- paste0("year: '", paste(bib$YEAR[[i]]), "'")
+  tit <- paste0("title: ", paste(bib$TITLE[[i]]))
+  jou <- paste0("journal: ", paste(bib$JOURNAL[[i]]))
+  vol <- paste0("volume: ", paste(bib$VOLUME[[i]]))
+  pge <- paste0("pages: ", paste(bib$PAGES[[i]]))
+  doi <- paste0("doi: ", paste(bib$DOI[[i]]))
+  url <- paste0("url: ", paste(bib$URL[[i]]))
+  name <- paste0("article", i, sep = "")
+
+  tmp <- list(typ = typ, col=col, au = au, yr = yr, tit = tit, jou = jou, vol = vol, pge = pge, doi = doi, url = url)
+  yamllist[[name]] <- tmp
+  
+
+  filename <- paste0(word(authorlist[1]), "_", bib$YEAR[[i]], "_", name, ".md")
+  abst <- paste0(bib$ABSTRACT[[i]])
+  tmp2 <- list(abst = abst, filename=filename)
+  yamllist2[[name]] <- tmp2
+}
+
+
+for (i in 1:length(yamllist)) {
+  fileConn<-file(yamllist2[[i]]$filename)
+  writeLines(c("---",
+               paste0(unlist(yamllist[i])),
+               "---",
+               paste0(yamllist2[[i]]$abst)), 
+             fileConn)
+  close(fileConn)
+}
+
+
+
+
+# for (i in 1:length(yamllist)) {
+#   cat(paste0("---", sep = "\n"),
+#       paste0(unlist(yamllist[i]), sep = "\n"), 
+#       paste0("---", sep = "\n"), 
+#       paste0("\n", yamllist2[[i]]$abst, sep = "\n"), file = yamllist2[[i]]$filename)
+# }
