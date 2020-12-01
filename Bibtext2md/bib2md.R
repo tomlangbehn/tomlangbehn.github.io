936f483a3e6c9ac7c6bf50eb36684edb6082c126
tomlangbehn <tom.langbehn@posteo.de>
2020-12-01 20:41:11 +0100

    pubs changed

--- Bibtext2md/bib2md.R
+++ Bibtext2md/bib2md.R
@@ -4,6 +4,10 @@ library(bib2df)
 #!> read bib text files
 bib <- bib2df::bib2df("my_pubs.bib") %>% arrange(YEAR,AUTHOR)
 
+pdf_filenames <- here::here("files","papers") %>% list.files(".pdf")
+
+
+
 
 yamllist <- list()
 yamllist2 <- list()
@@ -11,6 +15,7 @@ yamllist2 <- list()
 for (i in 1:nrow(bib)) {
   no_au <- length(bib$AUTHOR[[i]])
   
+  #!> reformat author names to just include lastname followed by initials
   authorlist <- bib$AUTHOR[[i]]
   for (j in 1:no_au) {
     lastname <- bib$AUTHOR[[i]][j] %>%
@@ -40,8 +45,16 @@ for (i in 1:nrow(bib)) {
   doi <- paste0("doi: ", paste(bib$DOI[[i]]))
   url <- paste0("url: ", bib$URL[[i]] %>% str_split(" ") %>% unlist %>% last())
   name <- paste0("article", i, sep = "")
+  head_title <- paste(bib$TITLE[[i]]) %>% str_trim() %>% word(., 1,6)
+  
+  if(pdf_filenames %>% str_detect(., head_title, negate = FALSE) %>% any %>% isTRUE){
+     pdf_file <- paste0("filename: ", pdf_filenames[pdf_filenames %>%  str_detect(., head_title, negate = FALSE)])
+  } else {
+    pdf_file <- paste0("filename:")
+  }
+  
+  tmp <- list(typ = typ, col=col, au = au, yr = yr, tit = tit, jou = jou, vol = vol, pge = pge, doi = doi, url = url, pdf_file=pdf_file)
 
-  tmp <- list(typ = typ, col=col, au = au, yr = yr, tit = tit, jou = jou, vol = vol, pge = pge, doi = doi, url = url)
   yamllist[[name]] <- tmp
   
 
